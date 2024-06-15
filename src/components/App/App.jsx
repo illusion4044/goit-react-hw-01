1b87aa376a639501c8f5a5d9bf954fc513ef02dc
Andrii <117983189+illusion4044@users.noreply.github.com>
2024-06-15 12:24:01 +0300

    minor update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,18 @@
+// {
+//     "username": "Jacques Gluke",
+//     "tag": "jgluke",
+//     "location": "Ocho Rios, Jamaica",
+//     "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+//     "stats": {
+//     "followers": 5603,
+//     "views": 4827,
+//     "likes": 1308
+//     }
+// }
+import ProfileSocial from "./ProfileSocial/ProfileSocial";
+
 export default function App() {
     return (
-        <div>
-        <p>Hello React !</p> 
-        </div>
+        <ProfileSocial />
     );
 }
---
+++ src/components/App/ProfileSocial/ProfileSocial.jsx
@@ -0,0 +1,9 @@
+export default function  ProfileSocial() {
+    return (
+        <div>
+        <p>Jacques Gluke</p>
+        <p>@jgluke</p>
+        <p>Ocho Rios, Jamaica</p>
+        </div>
+    );
+}
