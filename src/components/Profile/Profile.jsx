b0684ead41a32e59ab14be7aa58ad792823b78d3
Andrii <117983189+illusion4044@users.noreply.github.com>
2024-06-16 00:45:55 +0300

    Updated profile props

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,35 +2,32 @@ import css from "./Profile.module.css"
 
 console.log(css)
 
-export default function  Profile(props) {
-    console.log(props);
+export default function Profile(props) {
+    const { name, tag, location, image, stats } = props;
+    
     return (
-        <div className={css.profile}>
-         <div>
-         <img
-         src="https://cdn-icons-png.flaticon.com/512/2922/2922506.png"
-         alt="User avatar"
-     />
-        <p>Jacques Gluke</p>
-        <p>@jgluke</p>
-         <p>Ocho Rios, Jamaica</p>
-         </div>
-        
-         <ul>
-         <li>
-         <span>Followers</span>
-         <span>5603</span>
-         </li>
-         <li>
-         <span>Views</span>
-         <span>4827</span>
-         </li>
-         <li>
-        <span>Likes</span>
-         <span>1308</span>
-         </li>
-         </ul>
+      <div className= {css.profile}>
+        <div>
+          <img src={image} alt="User avatar" />
+          <p>{name}</p>
+          <p>{tag}</p>
+          <p>{location}</p>
         </div>
-        
+  
+        <ul>
+          <li>
+            <span>Followers</span>
+            <span>{stats.followers}</span>
+          </li>
+          <li>
+            <span>Views</span>
+            <span>{stats.views}</span>
+          </li>
+          <li>
+            <span>Likes</span>
+            <span>{stats.likes}</span>
+          </li>
+        </ul>
+      </div>
     );
-}
+  }
